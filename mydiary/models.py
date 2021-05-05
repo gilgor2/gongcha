f566df88ce6518cb1991e7b73e507ffd49be98b8
jaenny <hongryeon_21@naver.com>
2021-05-06 00:31:46 +0900

    ManyToOne session done

--- mydiary/models.py
+++ mydiary/models.py
@@ -3,6 +3,13 @@ from django.utils import timezone
 from django.db import models
 # Create your models here.
 class Content(models.Model):
+    objects = models.Manager() #에러를 피하기 위해 추가
     title = models.CharField(max_length=200)
     pub_date = models.DateTimeField(default = timezone.now)
     body = models.TextField(default='')
+
+class Comment(models.Model):
+    objects = models.Manager() #에러를 피하기 위해 추가
+    post = models.ForeignKey('Content',on_delete=models.CASCADE)
+    text = models.TextField(default='')
+    created_date = models.DateTimeField(default=timezone.now)
--- mydiary/views.py
+++ mydiary/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.utils import timezone
-from .models import Content
-from .forms import ContentForm
+from .models import Content, Comment
+from .forms import ContentForm, CommentForm
 # Create your views here.
 def home(request):
     posts = Content.objects.all()
@@ -20,9 +20,20 @@ def new(request):
         form = ContentForm()
     return render(request, 'new.html',{'form':form})
 
-def detail(request, index):
-    post = get_object_or_404(Content, pk = index)
-    return render(request, 'detail.html', {'post':post})
+def detail(request, pk):
+    post = get_object_or_404(Content, pk = pk)
+    comment_list = Comment.objects.filter(post=post)
+    if request.method == 'POST':
+        comment_form = CommentForm(request.POST)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.published_date = timezone.now()
+            comment.post = post
+            comment.save()
+            return redirect('detail', pk = pk)
+    else :
+        comment_form = CommentForm()
+    return render(request, 'detail.html', {'post':post, 'comment_list':comment_list, 'comment_form':comment_form})
 
 def edit(request, index):
     post = get_object_or_404(Content, pk = index)
@@ -33,7 +44,7 @@ def edit(request, index):
             post.author = request.user
             post.published_date = timezone.now
             post.save()
-            return redirect('detail', index=post.pk)
+            return redirect('detail', pk=post.pk)
     else:
         form = ContentForm(instance=post)
     return render(request, 'edit.html',{'form':form})
@@ -42,3 +53,8 @@ def delete(request,pk):
     post = get_object_or_404(Content, pk=pk)
     post.delete()
     return redirect('home')
+
+def delete_comment(request, pk, comment_pk) :
+    comment = get_object_or_404(Comment,pk=comment_pk)
+    comment.delete()
+    return redirect('detail',pk=pk)
