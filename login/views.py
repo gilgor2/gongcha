7c9b3883ae0a9dcebf9083eb72d383687ff30f6e
gilgor2 <misby0327@gmail.com>
2021-06-29 11:47:49 +0900

    login ver.1 (link)

--- login/views.py
+++ login/views.py
@@ -29,7 +29,7 @@ def login_view(request):
                 return redirect('home')
     else:
         form = AuthenticationForm()
-    return render(request, 'base.html', {'form':form}) 
+    return render(request, 'login.html', {'form':form}) 
 
 def logout_view(request):
 	logout(request)
