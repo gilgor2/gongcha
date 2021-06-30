4e04e9530f6f77c3ee115e5d8fbfe532a4b70bac
summerlunaa <rhgus1085@gmail.com>
2021-06-30 16:26:07 +0900

    requirements.txt

--- mydiary/models.py
+++ mydiary/models.py
@@ -1,6 +1,8 @@
 from __future__ import unicode_literals
 from django.utils import timezone
 from django.db import models
+
+
 # Create your models here.
 class Content(models.Model):
     objects = models.Manager() #에러를 피하기 위해 추가
